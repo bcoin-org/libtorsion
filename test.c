888855ce55218e60820d79ade16b214e32ccb00a
Christopher Jeffrey <chjjeffrey@gmail.com>
2020-01-06 12:13:56 -0800

    edwards mul tests

--- test.c
+++ test.c
@@ -655,7 +655,7 @@ test_wei_points_secp256k1(drbg_t *rng) {
 }
 
 static void
-test_wei_mul_g(drbg_t *rng) {
+test_wei_mul_g_p256(drbg_t *rng) {
   const unsigned char k_raw[32] = {
     0x38, 0xf8, 0x62, 0x0b, 0xa6, 0x0b, 0xed, 0x7c,
     0xf9, 0x0c, 0x7a, 0x99, 0xac, 0x35, 0xa4, 0x4e,
@@ -680,7 +680,7 @@ test_wei_mul_g(drbg_t *rng) {
   size_t q_size;
   scalar_field_t *sc = &ec->sc;
 
-  printf("Testing mul_g (vector).\n");
+  printf("Testing mul_g (vector, p256).\n");
 
   wei_init(ec, &curve_p256);
 
@@ -715,7 +715,7 @@ test_wei_mul_g(drbg_t *rng) {
 }
 
 static void
-test_wei_mul(drbg_t *rng) {
+test_wei_mul_p256(drbg_t *rng) {
   const unsigned char p_raw[33] = {
     0x03, 0x42, 0x67, 0xab, 0xc7, 0xde, 0x72, 0x0f,
     0x14, 0x5a, 0xbc, 0x94, 0xb9, 0x5b, 0x33, 0x50,
@@ -748,7 +748,7 @@ test_wei_mul(drbg_t *rng) {
   size_t q_size;
   scalar_field_t *sc = &ec->sc;
 
-  printf("Testing mul (vector).\n");
+  printf("Testing mul (vector, p256).\n");
 
   wei_init(ec, &curve_p256);
 
@@ -785,7 +785,7 @@ test_wei_mul(drbg_t *rng) {
 }
 
 static void
-test_wei_double_mul(drbg_t *rng) {
+test_wei_double_mul_p256(drbg_t *rng) {
   const unsigned char p_raw[33] = {
     0x02, 0x65, 0x26, 0x45, 0xad, 0x1a, 0x36, 0x8c,
     0xdc, 0xcf, 0x81, 0x90, 0x56, 0x3b, 0x2a, 0x12,
@@ -825,7 +825,7 @@ test_wei_double_mul(drbg_t *rng) {
   size_t q_size;
   scalar_field_t *sc = &ec->sc;
 
-  printf("Testing double mul (vector).\n");
+  printf("Testing double mul (vector, p256).\n");
 
   wei_init(ec, &curve_p256);
 
@@ -854,7 +854,7 @@ test_wei_double_mul(drbg_t *rng) {
 }
 
 static void
-test_wei_multi_mul(drbg_t *rng) {
+test_wei_multi_mul_p256(drbg_t *rng) {
   const unsigned char p1_raw[33] = {
     0x02, 0x2b, 0xbf, 0x66, 0x0c, 0x19, 0x5a, 0xea,
     0x82, 0x82, 0x2d, 0x2e, 0x69, 0xc4, 0x02, 0x6e,
@@ -912,7 +912,7 @@ test_wei_multi_mul(drbg_t *rng) {
   wge_t points[2];
   sc_t coeffs[2];
 
-  printf("Testing multi mul (vector).\n");
+  printf("Testing multi mul (vector, p256).\n");
 
   wei_init(ec, &curve_p256);
 
@@ -950,7 +950,7 @@ test_wei_multi_mul(drbg_t *rng) {
 }
 
 static void
-test_wei_mul_g2(drbg_t *rng) {
+test_wei_mul_g_secp256k1(drbg_t *rng) {
   const unsigned char k_raw[32] = {
     0xf7, 0x6c, 0xd0, 0xed, 0xfb, 0x89, 0x7f, 0x39,
     0xf6, 0x3e, 0x2c, 0x16, 0xc5, 0x73, 0x81, 0xb7,
@@ -1010,7 +1010,7 @@ test_wei_mul_g2(drbg_t *rng) {
 }
 
 static void
-test_wei_mul2(drbg_t *rng) {
+test_wei_mul_secp256k1(drbg_t *rng) {
   const unsigned char p_raw[33] = {
     0x02, 0x0a, 0xfc, 0xf3, 0x56, 0xdb, 0x98, 0x4f,
     0xa0, 0x33, 0x98, 0x35, 0xfe, 0xb4, 0xd9, 0x65,
@@ -1080,7 +1080,7 @@ test_wei_mul2(drbg_t *rng) {
 }
 
 static void
-test_wei_double_mul2(drbg_t *rng) {
+test_wei_double_mul_secp256k1(drbg_t *rng) {
   const unsigned char p_raw[33] = {
     0x02, 0x08, 0x0d, 0x86, 0xe7, 0x20, 0x89, 0x5f,
     0xde, 0x96, 0x2c, 0x69, 0x75, 0xff, 0x3a, 0x34,
@@ -1849,6 +1849,283 @@ test_edwards_points_ed25519(drbg_t *rng) {
 }
 
 static void
+test_edwards_mul_g_ed25519(drbg_t *rng) {
+  const unsigned char k_raw[32] = {
+    0x69, 0x33, 0xe6, 0x67, 0x60, 0xa1, 0x4f, 0x93,
+    0xa7, 0x75, 0x24, 0xb9, 0x07, 0xe4, 0x03, 0x75,
+    0xf5, 0x4f, 0xd6, 0x0d, 0xec, 0x03, 0x3f, 0x1b,
+    0x74, 0xf1, 0x54, 0x6d, 0xf2, 0xd7, 0xe8, 0x09
+  };
+
+  const unsigned char expect_raw[32] = {
+    0x3c, 0xcf, 0xe3, 0x87, 0xa1, 0x5e, 0x19, 0x62,
+    0x7e, 0xa5, 0x2f, 0x01, 0x3c, 0x42, 0x8e, 0xb3,
+    0x50, 0x7c, 0xa8, 0x2f, 0x19, 0x90, 0x61, 0xd2,
+    0xcb, 0xe1, 0x07, 0x8f, 0x01, 0x21, 0xaf, 0x21
+  };
+
+  edwards_t curve;
+  edwards_t *ec = &curve;
+  sc_t k;
+  ege_t q, expect;
+  unsigned char entropy[32];
+  unsigned char q_raw[32];
+  scalar_field_t *sc = &ec->sc;
+
+  printf("Testing mul_g (vector, ed25519).\n");
+
+  edwards_init(ec, &curve_ed25519);
+
+  random_bytes(rng, entropy, sizeof(entropy));
+
+  edwards_randomize(ec, entropy);
+
+  assert(sc_import(sc, k, k_raw));
+  assert(ege_import(ec, &expect, expect_raw));
+
+  assert(ege_validate(ec, &expect));
+  assert(ege_equal(ec, &expect, &expect));
+  assert(!ege_equal(ec, &expect, &ec->g));
+
+  edwards_mul_g(ec, &q, k);
+
+  assert(ege_equal(ec, &q, &expect));
+
+  ege_export(ec, q_raw, &q);
+
+  assert(memcmp(q_raw, expect_raw, 32) == 0);
+
+  edwards_mul_g_var(ec, &q, k);
+
+  assert(ege_equal(ec, &q, &expect));
+
+  ege_export(ec, q_raw, &q);
+
+  assert(memcmp(q_raw, expect_raw, 32) == 0);
+}
+
+static void
+test_edwards_mul_ed25519(drbg_t *rng) {
+  const unsigned char p_raw[32] = {
+    0xe1, 0xb0, 0x38, 0x25, 0x32, 0xd6, 0x5a, 0x8f,
+    0x8d, 0xb3, 0x9a, 0x07, 0xff, 0x2a, 0x80, 0x9c,
+    0x10, 0xbe, 0x39, 0xee, 0xf8, 0x6f, 0x85, 0x3b,
+    0x18, 0xcb, 0x8d, 0xff, 0x52, 0x5d, 0x18, 0xea
+  };
+
+  const unsigned char k_raw[32] = {
+    0x80, 0x41, 0xd8, 0xb0, 0xc9, 0x39, 0x74, 0x5f,
+    0xd7, 0x86, 0xa0, 0x39, 0x50, 0x5b, 0x60, 0x14,
+    0xf0, 0xa7, 0x2f, 0xf6, 0x54, 0x17, 0x82, 0x50,
+    0x02, 0xde, 0x22, 0xae, 0x95, 0xd6, 0x6f, 0x04
+  };
+
+  const unsigned char expect_raw[32] = {
+    0xca, 0x61, 0x5f, 0x44, 0xdf, 0xb4, 0xae, 0x00,
+    0x53, 0xff, 0x3d, 0xb7, 0x1f, 0x7a, 0xbe, 0xfd,
+    0x52, 0xf5, 0x8f, 0x36, 0x2c, 0xd5, 0x7c, 0x04,
+    0x68, 0x2a, 0xee, 0xb0, 0x12, 0xec, 0x90, 0xbe
+  };
+
+  edwards_t curve;
+  edwards_t *ec = &curve;
+  sc_t k;
+  ege_t p, q, expect;
+  unsigned char entropy[32];
+  unsigned char q_raw[32];
+  scalar_field_t *sc = &ec->sc;
+
+  printf("Testing mul (vector, ed25519).\n");
+
+  edwards_init(ec, &curve_ed25519);
+
+  random_bytes(rng, entropy, sizeof(entropy));
+
+  edwards_randomize(ec, entropy);
+
+  assert(ege_import(ec, &p, p_raw));
+  assert(sc_import(sc, k, k_raw));
+  assert(ege_import(ec, &expect, expect_raw));
+
+  assert(ege_validate(ec, &p));
+  assert(ege_validate(ec, &expect));
+  assert(ege_equal(ec, &expect, &expect));
+  assert(!ege_equal(ec, &expect, &ec->g));
+
+  edwards_mul(ec, &q, &p, k);
+
+  assert(ege_equal(ec, &q, &expect));
+
+  ege_export(ec, q_raw, &q);
+
+  assert(memcmp(q_raw, expect_raw, 32) == 0);
+
+  edwards_mul_var(ec, &q, &p, k);
+
+  assert(ege_equal(ec, &q, &expect));
+
+  ege_export(ec, q_raw, &q);
+
+  assert(memcmp(q_raw, expect_raw, 32) == 0);
+}
+
+static void
+test_edwards_double_mul_ed25519(drbg_t *rng) {
+  const unsigned char p_raw[32] = {
+    0x3e, 0x5f, 0x12, 0xb3, 0xec, 0x9e, 0x60, 0x1c,
+    0x2b, 0x6c, 0x5b, 0x5b, 0xe6, 0x31, 0x97, 0xc0,
+    0xc0, 0xef, 0x70, 0x61, 0x0a, 0xa6, 0x59, 0xef,
+    0x1c, 0xdd, 0x6d, 0x0d, 0xac, 0x5b, 0x75, 0x5d
+  };
+
+  const unsigned char k1_raw[32] = {
+    0xaa, 0x78, 0x33, 0xea, 0x1b, 0x8c, 0xd3, 0x7e,
+    0x8c, 0x4b, 0xbc, 0xea, 0x75, 0x88, 0x53, 0x1d,
+    0x45, 0x11, 0x5c, 0x03, 0x4f, 0xee, 0xfc, 0x98,
+    0x51, 0x54, 0x37, 0x3e, 0xe5, 0x59, 0xe8, 0x03
+  };
+
+  const unsigned char k2_raw[32] = {
+    0x87, 0xd3, 0x3b, 0x4a, 0x4c, 0x11, 0x08, 0xf2,
+    0x2d, 0x6f, 0x53, 0xd5, 0xfd, 0x7a, 0x14, 0x8e,
+    0xc3, 0x68, 0x63, 0x6d, 0xc1, 0x80, 0x0d, 0xd9,
+    0x28, 0xd7, 0x05, 0x49, 0x18, 0x0c, 0x9c, 0x01
+  };
+
+  const unsigned char expect_raw[32] = {
+    0x1c, 0x54, 0xae, 0xd8, 0xa6, 0x76, 0x2d, 0xde,
+    0x34, 0x28, 0xe8, 0xdc, 0x59, 0xdf, 0x54, 0x29,
+    0x92, 0x96, 0xb3, 0xb6, 0xa5, 0xb6, 0x7c, 0x78,
+    0xf9, 0xc3, 0x7b, 0xb1, 0x26, 0xa3, 0x7f, 0x50
+  };
+
+  edwards_t curve;
+  edwards_t *ec = &curve;
+  sc_t k1, k2;
+  ege_t p, q, expect;
+  unsigned char entropy[32];
+  unsigned char q_raw[32];
+  scalar_field_t *sc = &ec->sc;
+
+  printf("Testing double mul (vector, ed25519).\n");
+
+  edwards_init(ec, &curve_ed25519);
+
+  random_bytes(rng, entropy, sizeof(entropy));
+
+  edwards_randomize(ec, entropy);
+
+  assert(ege_import(ec, &p, p_raw));
+  assert(sc_import(sc, k1, k1_raw));
+  assert(sc_import(sc, k2, k2_raw));
+  assert(ege_import(ec, &expect, expect_raw));
+
+  assert(ege_validate(ec, &p));
+  assert(ege_validate(ec, &expect));
+  assert(ege_equal(ec, &expect, &expect));
+  assert(!ege_equal(ec, &expect, &ec->g));
+
+  edwards_mul_double_var(ec, &q, k1, &p, k2);
+
+  assert(ege_equal(ec, &q, &expect));
+
+  ege_export(ec, q_raw, &q);
+
+  assert(memcmp(q_raw, expect_raw, 32) == 0);
+}
+
+static void
+test_edwards_multi_mul_ed25519(drbg_t *rng) {
+  const unsigned char p1_raw[32] = {
+    0xfa, 0xa1, 0xa7, 0xbc, 0xc6, 0x40, 0x81, 0xe9,
+    0xc4, 0x9b, 0x28, 0x49, 0x8a, 0x99, 0x50, 0xbb,
+    0x58, 0x11, 0x4a, 0xd0, 0xf6, 0xf9, 0x1e, 0x11,
+    0xfb, 0xf1, 0xce, 0xb3, 0x28, 0xf6, 0xac, 0xce
+  };
+
+  const unsigned char p2_raw[32] = {
+    0x70, 0x21, 0x0f, 0xd8, 0xd5, 0x32, 0x0f, 0xcc,
+    0xd1, 0x9e, 0x97, 0x96, 0x4f, 0x58, 0xd2, 0xf8,
+    0x1e, 0x3c, 0x3e, 0x14, 0xfa, 0xed, 0xaa, 0xe2,
+    0x32, 0x8f, 0x5d, 0x9a, 0x4e, 0x86, 0x5d, 0xc1
+  };
+
+  const unsigned char k0_raw[32] = {
+    0x1b, 0xaf, 0x38, 0x8f, 0x81, 0x39, 0x2c, 0xb6,
+    0x9d, 0xfe, 0xd8, 0xca, 0x76, 0x3d, 0xfd, 0xa0,
+    0xb0, 0x3a, 0x90, 0xfa, 0xf3, 0x76, 0xd2, 0x94,
+    0xff, 0x2a, 0x57, 0x44, 0xf3, 0x36, 0x59, 0x0f
+  };
+
+  const unsigned char k1_raw[32] = {
+    0xa9, 0x4e, 0x67, 0x1e, 0xd0, 0xee, 0xa4, 0xe0,
+    0x96, 0xd4, 0xa9, 0x10, 0x84, 0x28, 0xbb, 0x03,
+    0x58, 0x00, 0x1c, 0xc0, 0x4a, 0x02, 0xd9, 0x02,
+    0x75, 0xf9, 0x88, 0x94, 0x56, 0x06, 0x49, 0x0f
+  };
+
+  const unsigned char k2_raw[32] = {
+    0xc3, 0x0f, 0xd1, 0x50, 0x6f, 0x29, 0x89, 0x7f,
+    0x6e, 0xc8, 0x52, 0x04, 0x9e, 0xa4, 0x9f, 0xb0,
+    0x46, 0x7e, 0xb2, 0x17, 0xb8, 0x4c, 0xd2, 0xbe,
+    0x4e, 0xac, 0xd1, 0xe3, 0xeb, 0x7f, 0xda, 0x02
+  };
+
+  const unsigned char expect_raw[32] = {
+    0x4f, 0x05, 0x88, 0x00, 0xfc, 0x45, 0xcb, 0xfe,
+    0x75, 0x06, 0x48, 0x21, 0x23, 0xc6, 0xf7, 0x4e,
+    0xbd, 0x44, 0x4f, 0x62, 0x4b, 0x6e, 0x28, 0x6a,
+    0x36, 0x1d, 0x25, 0xea, 0x2a, 0xd5, 0x48, 0x86
+  };
+
+  edwards_t curve;
+  edwards_t *ec = &curve;
+  sc_t k0, k1, k2;
+  ege_t p1, p2, q, expect;
+  unsigned char entropy[32];
+  unsigned char q_raw[32];
+  scalar_field_t *sc = &ec->sc;
+  edwards_scratch_t scratch;
+  ege_t points[2];
+  sc_t coeffs[2];
+
+  printf("Testing multi mul (vector).\n");
+
+  edwards_init(ec, &curve_ed25519);
+
+  random_bytes(rng, entropy, sizeof(entropy));
+
+  edwards_randomize(ec, entropy);
+
+  assert(ege_import(ec, &p1, p1_raw));
+  assert(ege_import(ec, &p2, p2_raw));
+  assert(sc_import(sc, k0, k0_raw));
+  assert(sc_import(sc, k1, k1_raw));
+  assert(sc_import(sc, k2, k2_raw));
+  assert(ege_import(ec, &expect, expect_raw));
+
+  assert(ege_validate(ec, &p1));
+  assert(ege_validate(ec, &p2));
+  assert(ege_validate(ec, &expect));
+  assert(ege_equal(ec, &expect, &expect));
+  assert(!ege_equal(ec, &expect, &ec->g));
+
+  ege_set(ec, &points[0], &p1);
+  ege_set(ec, &points[1], &p2);
+
+  sc_set(sc, coeffs[0], k1);
+  sc_set(sc, coeffs[1], k2);
+
+  edwards_mul_multi_var(ec, &q, k0, points, coeffs, 2, &scratch);
+
+  assert(ege_equal(ec, &q, &expect));
+
+  ege_export(ec, q_raw, &q);
+
+  assert(memcmp(q_raw, expect_raw, 32) == 0);
+}
+
+static void
 test_eddsa_vector_ed25519(drbg_t *rng) {
   const unsigned char priv[32] = {
     0xd7, 0x4c, 0x01, 0x53, 0xc5, 0xcd, 0xf4, 0x8b,
@@ -2314,13 +2591,13 @@ main(int argc, char **argv) {
     test_wei_points_p256(&rng);
     test_wei_points_p521(&rng);
     test_wei_points_secp256k1(&rng);
-    test_wei_mul_g(&rng);
-    test_wei_mul(&rng);
-    test_wei_double_mul(&rng);
-    test_wei_multi_mul(&rng);
-    test_wei_mul_g2(&rng);
-    test_wei_mul2(&rng);
-    test_wei_double_mul2(&rng);
+    test_wei_mul_g_p256(&rng);
+    test_wei_mul_p256(&rng);
+    test_wei_double_mul_p256(&rng);
+    test_wei_multi_mul_p256(&rng);
+    test_wei_mul_g_secp256k1(&rng);
+    test_wei_mul_secp256k1(&rng);
+    test_wei_double_mul_secp256k1(&rng);
     test_ecdsa_vector_p224(&rng);
     test_ecdsa_vector_p256(&rng);
     test_ecdsa_vector_p384(&rng);
@@ -2330,6 +2607,10 @@ main(int argc, char **argv) {
     test_ecdh_vector_x25519();
     test_ecdh_vector_x448();
     test_edwards_points_ed25519(&rng);
+    test_edwards_mul_g_ed25519(&rng);
+    test_edwards_mul_ed25519(&rng);
+    test_edwards_double_mul_ed25519(&rng);
+    test_edwards_multi_mul_ed25519(&rng);
     test_eddsa_vector_ed25519(&rng);
     test_eddsa_vector_ed448(&rng);
     test_ecdsa_random(&rng);
