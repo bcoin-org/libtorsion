039efe130cadfd5524dc6732a298524110a37fb6
Christopher Jeffrey <chjjeffrey@gmail.com>
2020-03-05 12:07:46 -0800

    valgrind: minor.

--- valgrind.sh
+++ valgrind.sh
@@ -2,4 +2,4 @@
 
 set -ex
 export LD_LIBRARY_PATH=./
-exec valgrind --track-origins=yes --leak-check=full --show-leak-kinds=all "$@" ./test "$@"
+exec valgrind --track-origins=yes --leak-check=full --show-leak-kinds=all ./test "$@"
