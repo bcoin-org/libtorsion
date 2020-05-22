b68c58e326d6a77d681d21b49858e303b904a89c
Christopher Jeffrey <chjjeffrey@gmail.com>
2020-05-22 03:15:00 -0700

    pkg: remove unnecessary files.

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -ex
-export LD_LIBRARY_PATH=./.libs
-exec ./tests "$@"
--- valgrind.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -ex
-export LD_LIBRARY_PATH=./.libs
-exec valgrind \
-  --track-origins=yes \
-  --leak-check=full \
-  --show-leak-kinds=all \
-  ./tests "$@"
