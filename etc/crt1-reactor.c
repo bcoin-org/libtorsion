4851f81ca7b478e04705e1904079d9f705247341
Christopher Jeffrey <chjjeffrey@gmail.com>
2020-07-29 11:15:16 -0700

    build: improvements for wasi build and unix makefile.

--- etc/crt1-reactor.c
+++
@@ -1,9 +0,0 @@
-/* See: https://github.com/WebAssembly/wasi-libc/blob/a6d0b4c/libc-bottom-half/crt/crt1-reactor.c */
-
-extern void __wasm_call_ctors(void);
-
-__attribute__((export_name("_initialize"))) void
-_initialize(void) {
-  /* The linker synthesizes this to call constructors. */
-  __wasm_call_ctors();
-}
