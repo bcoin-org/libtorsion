ede44304f74dfd64c014bef5a6a1afe30ec6dc17
Christopher Jeffrey <chjjeffrey@gmail.com>
2020-12-19 17:22:52 -0800

    src/hash: rewrite md5. remove nettle code.

--- src/hash.c
+++ src/hash.c
@@ -1177,8 +1177,8 @@ md4_init(md4_t *ctx) {
 
 static void
 md4_transform(md4_t *ctx, const unsigned char *chunk) {
-  uint32_t W[16];
   uint32_t a, b, c, d;
+  uint32_t W[16];
   int i;
 
   for (i = 0; i < 16; i++)
@@ -1192,29 +1192,29 @@ md4_transform(md4_t *ctx, const unsigned char *chunk) {
 #define F(x, y, z) ((x & y) | (~x & z))
 #define G(x, y, z) ((x & y) | (x & z) | (y & z))
 #define H(x, y, z) (x ^ y ^ z)
-#define O1(a, b, c, d, k, s) ROTL32N(a + F(b, c, d) + k, s)
-#define O2(a, b, c, d, k, s) ROTL32N(a + G(b, c, d) + k + 0x5a827999, s)
-#define O3(a, b, c, d, k, s) ROTL32N(a + H(b, c, d) + k + 0x6ed9eba1, s)
+#define F1(a, b, c, d, k, s) ROTL32N(a + F(b, c, d) + k, s)
+#define F2(a, b, c, d, k, s) ROTL32N(a + G(b, c, d) + k + 0x5a827999, s)
+#define F3(a, b, c, d, k, s) ROTL32N(a + H(b, c, d) + k + 0x6ed9eba1, s)
 
 #define R1(i) do {                   \
-  a = O1(a, b, c, d, W[i +  0],  3); \
-  d = O1(d, a, b, c, W[i +  1],  7); \
-  c = O1(c, d, a, b, W[i +  2], 11); \
-  b = O1(b, c, d, a, W[i +  3], 19); \
+  a = F1(a, b, c, d, W[i +  0],  3); \
+  d = F1(d, a, b, c, W[i +  1],  7); \
+  c = F1(c, d, a, b, W[i +  2], 11); \
+  b = F1(b, c, d, a, W[i +  3], 19); \
 } while (0)
 
 #define R2(i) do {                   \
-  a = O2(a, b, c, d, W[i +  0],  3); \
-  d = O2(d, a, b, c, W[i +  4],  5); \
-  c = O2(c, d, a, b, W[i +  8],  9); \
-  b = O2(b, c, d, a, W[i + 12], 13); \
+  a = F2(a, b, c, d, W[i +  0],  3); \
+  d = F2(d, a, b, c, W[i +  4],  5); \
+  c = F2(c, d, a, b, W[i +  8],  9); \
+  b = F2(b, c, d, a, W[i + 12], 13); \
 } while (0)
 
 #define R3(i) do {                   \
-  a = O3(a, b, c, d, W[i +  0],  3); \
-  d = O3(d, a, b, c, W[i +  8],  9); \
-  c = O3(c, d, a, b, W[i +  4], 11); \
-  b = O3(b, c, d, a, W[i + 12], 15); \
+  a = F3(a, b, c, d, W[i +  0],  3); \
+  d = F3(d, a, b, c, W[i +  8],  9); \
+  c = F3(c, d, a, b, W[i +  4], 11); \
+  b = F3(b, c, d, a, W[i + 12], 15); \
 } while (0)
 
   /* Round 1. */
@@ -1238,9 +1238,9 @@ md4_transform(md4_t *ctx, const unsigned char *chunk) {
 #undef F
 #undef G
 #undef H
-#undef O1
-#undef O2
-#undef O3
+#undef F1
+#undef F2
+#undef F3
 #undef R1
 #undef R2
 #undef R3
@@ -1312,7 +1312,6 @@ md4_final(md4_t *ctx, unsigned char *out) {
  * Resources:
  *   https://en.wikipedia.org/wiki/MD5
  *   https://tools.ietf.org/html/rfc1321
- *   https://github.com/gnutls/nettle/blob/master/md5-compress.c
  */
 
 static const unsigned char md5_P[64] = {
@@ -1337,92 +1336,97 @@ md5_init(md5_t *ctx) {
 
 static void
 md5_transform(md5_t *ctx, const unsigned char *chunk) {
-  uint32_t data[16];
-  uint32_t a, b, c, d;
+  uint32_t a, b, c, d, f;
+  uint32_t W[16];
   int i;
 
-  for (i = 0; i < 16; i++, chunk += 4)
-    data[i] = read32le(chunk);
+  for (i = 0; i < 16; i++)
+    W[i] = read32le(chunk + i * 4);
 
   a = ctx->state[0];
   b = ctx->state[1];
   c = ctx->state[2];
   d = ctx->state[3];
 
-#define F1(x, y, z) ((z) ^ ((x) & ((y) ^ (z))))
-#define F2(x, y, z) F1((z), (x), (y))
-#define F3(x, y, z) ((x) ^ (y) ^ (z))
-#define F4(x, y, z) ((y) ^ ((x) | ~(z)))
-#define ROUND(f, w, x, y, z, data, s) \
-  (w += f(x, y, z) + data, w = w << s | w >> (32 - s), w += x)
+#define F1 ((b & c) | (~b & d))
+#define F2 ((d & b) | (~d & c))
+#define F3 (b ^ c ^ d)
+#define F4 (c ^ (b | ~d))
+#define ROUND(F, g, s, k) do { \
+  f = F + a + k + W[g];        \
+  a = d;                       \
+  d = c;                       \
+  c = b;                       \
+  b += ROTL32N(f, s);          \
+} while (0)
 
-  ROUND(F1, a, b, c, d, data[ 0] + 0xd76aa478, 7);
-  ROUND(F1, d, a, b, c, data[ 1] + 0xe8c7b756, 12);
-  ROUND(F1, c, d, a, b, data[ 2] + 0x242070db, 17);
-  ROUND(F1, b, c, d, a, data[ 3] + 0xc1bdceee, 22);
-  ROUND(F1, a, b, c, d, data[ 4] + 0xf57c0faf, 7);
-  ROUND(F1, d, a, b, c, data[ 5] + 0x4787c62a, 12);
-  ROUND(F1, c, d, a, b, data[ 6] + 0xa8304613, 17);
-  ROUND(F1, b, c, d, a, data[ 7] + 0xfd469501, 22);
-  ROUND(F1, a, b, c, d, data[ 8] + 0x698098d8, 7);
-  ROUND(F1, d, a, b, c, data[ 9] + 0x8b44f7af, 12);
-  ROUND(F1, c, d, a, b, data[10] + 0xffff5bb1, 17);
-  ROUND(F1, b, c, d, a, data[11] + 0x895cd7be, 22);
-  ROUND(F1, a, b, c, d, data[12] + 0x6b901122, 7);
-  ROUND(F1, d, a, b, c, data[13] + 0xfd987193, 12);
-  ROUND(F1, c, d, a, b, data[14] + 0xa679438e, 17);
-  ROUND(F1, b, c, d, a, data[15] + 0x49b40821, 22);
+  ROUND(F1,  0,  7, 0xd76aa478);
+  ROUND(F1,  1, 12, 0xe8c7b756);
+  ROUND(F1,  2, 17, 0x242070db);
+  ROUND(F1,  3, 22, 0xc1bdceee);
+  ROUND(F1,  4,  7, 0xf57c0faf);
+  ROUND(F1,  5, 12, 0x4787c62a);
+  ROUND(F1,  6, 17, 0xa8304613);
+  ROUND(F1,  7, 22, 0xfd469501);
+  ROUND(F1,  8,  7, 0x698098d8);
+  ROUND(F1,  9, 12, 0x8b44f7af);
+  ROUND(F1, 10, 17, 0xffff5bb1);
+  ROUND(F1, 11, 22, 0x895cd7be);
+  ROUND(F1, 12,  7, 0x6b901122);
+  ROUND(F1, 13, 12, 0xfd987193);
+  ROUND(F1, 14, 17, 0xa679438e);
+  ROUND(F1, 15, 22, 0x49b40821);
 
-  ROUND(F2, a, b, c, d, data[ 1] + 0xf61e2562, 5);
-  ROUND(F2, d, a, b, c, data[ 6] + 0xc040b340, 9);
-  ROUND(F2, c, d, a, b, data[11] + 0x265e5a51, 14);
-  ROUND(F2, b, c, d, a, data[ 0] + 0xe9b6c7aa, 20);
-  ROUND(F2, a, b, c, d, data[ 5] + 0xd62f105d, 5);
-  ROUND(F2, d, a, b, c, data[10] + 0x02441453, 9);
-  ROUND(F2, c, d, a, b, data[15] + 0xd8a1e681, 14);
-  ROUND(F2, b, c, d, a, data[ 4] + 0xe7d3fbc8, 20);
-  ROUND(F2, a, b, c, d, data[ 9] + 0x21e1cde6, 5);
-  ROUND(F2, d, a, b, c, data[14] + 0xc33707d6, 9);
-  ROUND(F2, c, d, a, b, data[ 3] + 0xf4d50d87, 14);
-  ROUND(F2, b, c, d, a, data[ 8] + 0x455a14ed, 20);
-  ROUND(F2, a, b, c, d, data[13] + 0xa9e3e905, 5);
-  ROUND(F2, d, a, b, c, data[ 2] + 0xfcefa3f8, 9);
-  ROUND(F2, c, d, a, b, data[ 7] + 0x676f02d9, 14);
-  ROUND(F2, b, c, d, a, data[12] + 0x8d2a4c8a, 20);
+  ROUND(F2,  1,  5, 0xf61e2562);
+  ROUND(F2,  6,  9, 0xc040b340);
+  ROUND(F2, 11, 14, 0x265e5a51);
+  ROUND(F2,  0, 20, 0xe9b6c7aa);
+  ROUND(F2,  5,  5, 0xd62f105d);
+  ROUND(F2, 10,  9, 0x02441453);
+  ROUND(F2, 15, 14, 0xd8a1e681);
+  ROUND(F2,  4, 20, 0xe7d3fbc8);
+  ROUND(F2,  9,  5, 0x21e1cde6);
+  ROUND(F2, 14,  9, 0xc33707d6);
+  ROUND(F2,  3, 14, 0xf4d50d87);
+  ROUND(F2,  8, 20, 0x455a14ed);
+  ROUND(F2, 13,  5, 0xa9e3e905);
+  ROUND(F2,  2,  9, 0xfcefa3f8);
+  ROUND(F2,  7, 14, 0x676f02d9);
+  ROUND(F2, 12, 20, 0x8d2a4c8a);
 
-  ROUND(F3, a, b, c, d, data[ 5] + 0xfffa3942, 4);
-  ROUND(F3, d, a, b, c, data[ 8] + 0x8771f681, 11);
-  ROUND(F3, c, d, a, b, data[11] + 0x6d9d6122, 16);
-  ROUND(F3, b, c, d, a, data[14] + 0xfde5380c, 23);
-  ROUND(F3, a, b, c, d, data[ 1] + 0xa4beea44, 4);
-  ROUND(F3, d, a, b, c, data[ 4] + 0x4bdecfa9, 11);
-  ROUND(F3, c, d, a, b, data[ 7] + 0xf6bb4b60, 16);
-  ROUND(F3, b, c, d, a, data[10] + 0xbebfbc70, 23);
-  ROUND(F3, a, b, c, d, data[13] + 0x289b7ec6, 4);
-  ROUND(F3, d, a, b, c, data[ 0] + 0xeaa127fa, 11);
-  ROUND(F3, c, d, a, b, data[ 3] + 0xd4ef3085, 16);
-  ROUND(F3, b, c, d, a, data[ 6] + 0x04881d05, 23);
-  ROUND(F3, a, b, c, d, data[ 9] + 0xd9d4d039, 4);
-  ROUND(F3, d, a, b, c, data[12] + 0xe6db99e5, 11);
-  ROUND(F3, c, d, a, b, data[15] + 0x1fa27cf8, 16);
-  ROUND(F3, b, c, d, a, data[ 2] + 0xc4ac5665, 23);
+  ROUND(F3,  5,  4, 0xfffa3942);
+  ROUND(F3,  8, 11, 0x8771f681);
+  ROUND(F3, 11, 16, 0x6d9d6122);
+  ROUND(F3, 14, 23, 0xfde5380c);
+  ROUND(F3,  1,  4, 0xa4beea44);
+  ROUND(F3,  4, 11, 0x4bdecfa9);
+  ROUND(F3,  7, 16, 0xf6bb4b60);
+  ROUND(F3, 10, 23, 0xbebfbc70);
+  ROUND(F3, 13,  4, 0x289b7ec6);
+  ROUND(F3,  0, 11, 0xeaa127fa);
+  ROUND(F3,  3, 16, 0xd4ef3085);
+  ROUND(F3,  6, 23, 0x04881d05);
+  ROUND(F3,  9,  4, 0xd9d4d039);
+  ROUND(F3, 12, 11, 0xe6db99e5);
+  ROUND(F3, 15, 16, 0x1fa27cf8);
+  ROUND(F3,  2, 23, 0xc4ac5665);
 
-  ROUND(F4, a, b, c, d, data[ 0] + 0xf4292244, 6);
-  ROUND(F4, d, a, b, c, data[ 7] + 0x432aff97, 10);
-  ROUND(F4, c, d, a, b, data[14] + 0xab9423a7, 15);
-  ROUND(F4, b, c, d, a, data[ 5] + 0xfc93a039, 21);
-  ROUND(F4, a, b, c, d, data[12] + 0x655b59c3, 6);
-  ROUND(F4, d, a, b, c, data[ 3] + 0x8f0ccc92, 10);
-  ROUND(F4, c, d, a, b, data[10] + 0xffeff47d, 15);
-  ROUND(F4, b, c, d, a, data[ 1] + 0x85845dd1, 21);
-  ROUND(F4, a, b, c, d, data[ 8] + 0x6fa87e4f, 6);
-  ROUND(F4, d, a, b, c, data[15] + 0xfe2ce6e0, 10);
-  ROUND(F4, c, d, a, b, data[ 6] + 0xa3014314, 15);
-  ROUND(F4, b, c, d, a, data[13] + 0x4e0811a1, 21);
-  ROUND(F4, a, b, c, d, data[ 4] + 0xf7537e82, 6);
-  ROUND(F4, d, a, b, c, data[11] + 0xbd3af235, 10);
-  ROUND(F4, c, d, a, b, data[ 2] + 0x2ad7d2bb, 15);
-  ROUND(F4, b, c, d, a, data[ 9] + 0xeb86d391, 21);
+  ROUND(F4,  0,  6, 0xf4292244);
+  ROUND(F4,  7, 10, 0x432aff97);
+  ROUND(F4, 14, 15, 0xab9423a7);
+  ROUND(F4,  5, 21, 0xfc93a039);
+  ROUND(F4, 12,  6, 0x655b59c3);
+  ROUND(F4,  3, 10, 0x8f0ccc92);
+  ROUND(F4, 10, 15, 0xffeff47d);
+  ROUND(F4,  1, 21, 0x85845dd1);
+  ROUND(F4,  8,  6, 0x6fa87e4f);
+  ROUND(F4, 15, 10, 0xfe2ce6e0);
+  ROUND(F4,  6, 15, 0xa3014314);
+  ROUND(F4, 13, 21, 0x4e0811a1);
+  ROUND(F4,  4,  6, 0xf7537e82);
+  ROUND(F4, 11, 10, 0xbd3af235);
+  ROUND(F4,  2, 15, 0x2ad7d2bb);
+  ROUND(F4,  9, 21, 0xeb86d391);
 
 #undef F1
 #undef F2
