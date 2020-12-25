95ebcbfe3b3fbc597e6c2661a73e52da5edf510f
Christopher Jeffrey <chjjeffrey@gmail.com>
2020-12-25 12:59:33 -0800

    src/kdf: do not assume SIZE_MAX is suitable for #if directives.

--- src/kdf.c
+++ src/kdf.c
@@ -928,6 +928,7 @@ scrypt_derive(unsigned char *out,
               uint32_t r,
               uint32_t p,
               size_t len) {
+  uint64_t len64 = len;
   int t = HASH_SHA256;
   uint8_t *B = NULL;
   uint8_t *V = NULL;
@@ -940,10 +941,8 @@ scrypt_derive(unsigned char *out,
   if (N == 0 || R == 0 || P == 0)
     return 0;
 
-#if SIZE_MAX > UINT32_MAX
-  if ((uint64_t)len > ((UINT64_C(1) << 32) - 1) * 32)
+  if (len64 > ((UINT64_C(1) << 32) - 1) * 32)
     return 0;
-#endif
 
   if ((uint64_t)R * (uint64_t)P >= (UINT64_C(1) << 30))
     return 0;
