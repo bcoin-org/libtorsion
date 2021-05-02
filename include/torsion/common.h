896077b2e1066afc545ba9fb569266a2addc56bf
Christopher Jeffrey <chjjeffrey@gmail.com>
2021-05-02 03:00:47 -0700

    include/common: fallback for when __GNUC_MINOR__ is not present.

--- include/torsion/common.h
+++ include/torsion/common.h
@@ -16,10 +16,12 @@
 #  elif defined(_MSC_VER) || defined(__BORLANDC__) \
      || defined(__WATCOMC__) || defined(__DMC__)
 #    define TORSION_EXTERN __declspec(dllexport)
-#  elif defined(__GNUC__)
+#  elif defined(__GNUC__) && defined(__GNUC_MINOR__)
 #    if ((__GNUC__ << 16) + __GNUC_MINOR__ >= 0x30003) /* 3.3 */
 #      define TORSION_EXTERN __attribute__((visibility("default")))
 #    endif
+#  elif defined(__GNUC__) && __GNUC__ >= 4 /* 4.0 */
+#    define TORSION_EXTERN __attribute__((visibility("default")))
 #  elif defined(__SUNPRO_C) && __SUNPRO_C >= 0x590 /* 5.9 */
 #    define TORSION_EXTERN __attribute__((visibility("default")))
 #  elif defined(__SUNPRO_C) && __SUNPRO_C >= 0x550 /* 5.5 */
