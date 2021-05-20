7892448463e91397c8836e9404495198842e5f22
Christopher Jeffrey <chjjeffrey@gmail.com>
2021-05-19 23:29:19 -0700

    entropy/posix: improve explanation. some cleanup.

--- src/entropy/posix.h
+++ src/entropy/posix.h
@@ -7,55 +7,73 @@
 #ifndef TORSION_POSIX_H
 #define TORSION_POSIX_H
 
-/* We need to do this because:
+/**
+ * Feature Test Macros
  *
- * 1. A lot of feature testing code in C standard libraries
- *    disables almost every POSIX feature if __STRICT_ANSI__
- *    (or __STDC__ == 1) is defined.
+ * Several C standard libraries determine the set of POSIX
+ * features based on the language standard. In particular,
+ * a number of them disable almost every feature if
+ * `__STRICT_ANSI__` (or `__STDC__=1`) is defined.
  *
- * 2. Some OSes won't even work at all without _POSIX_SOURCE
- *    defined.
+ * This includes (but is probably not limited to): glibc,
+ * sun/solaris, cygwin, hpux, qnx, and haiku (sort of).
  *
- * 3. Some OSes just have really bad defaults.
+ * I'm uncertain whether the IBM OSes base their POSIX
+ * features on the language standard, but they're
+ * nevertheless some of the worst offenders (especially
+ * z/OS) in that they require a feature test macro for
+ * damn near everything (including POSIX.1).
+ *
+ * In my opinion, this is one aspect of C that the BSDs
+ * get right: they don't go in for any of this nonsense
+ * (the default always includes everything).
+ *
+ * GCC and Clang try to fix a lot of this on their own,
+ * especially when compiling C++, but they fall short for
+ * our needs.
+ *
+ * We want our code to be able to access the same POSIX
+ * APIs regardless of the language standard chosen.
  *
  * For reference:
  *
- *   - glibc: https://github.com/bminor/glibc/blob/6c57d32/include/features.h
- *   - bionic: https://github.com/aosp-mirror/platform_bionic/blob/a1112fd/libc/include/sys/cdefs.h#L162
- *   - musl: https://github.com/ifduyue/musl/blob/1febd21/include/features.h
- *   - cygwin: https://github.com/cygwin/cygwin/blob/8050ef2/newlib/libc/include/sys/features.h
- *   - darwin: https://github.com/apple/darwin-xnu/blob/d4061fb/bsd/sys/cdefs.h#L792
- *   - freebsd: https://github.com/freebsd/freebsd-src/blob/cfad8bd/sys/sys/cdefs.h#L638
- *   - openbsd: https://github.com/openbsd/src/blob/43b1a0f/sys/sys/cdefs.h#L261
- *   - netbsd: https://github.com/NetBSD/src/blob/ee650a6/sys/sys/featuretest.h
- *   - dragonfly: https://github.com/DragonFlyBSD/DragonFlyBSD/blob/c97dc9d/sys/sys/cdefs.h#L597
- *   - sun: https://docs.oracle.com/cd/E19253-01/816-5175/standards-5/index.html
- *          https://github.com/illumos/illumos-gate/blob/9ecd05b/usr/src/uts/common/sys/feature_tests.h
- *          https://github.com/illumos/illumos-gate/blob/9ecd05b/usr/src/uts/common/sys/unistd.h
- *   - hpux: https://nixdoc.net/man-pages/HP-UX/man5/stdsyms.5.html
- *   - aix: ??
- *   - z/os: https://www.ibm.com/docs/en/zos/2.3.0?topic=files-feature-test-macros
- *   - qnx: http://www.qnx.com/developers/docs/6.5.0_sp1/topic/com.qnx.doc.neutrino_prog/devel.html
-            https://github.com/vocho/openqnx/blob/cc95df3/trunk/lib/c/public/sys/platform.h
- *   - haiku: https://github.com/haiku/haiku/blob/144f45a/headers/compatibility/bsd/features.h
- *            https://github.com/haiku/haiku/blob/144f45a/headers/posix/unistd.h
- *   - minix: https://github.com/Stichting-MINIX-Research-Foundation/minix/blob/4db99f4/sys/sys/featuretest.h
- *   - redox: https://github.com/redox-os/relibc/blob/9790289/include/bits/unistd.h
- *   - vxworks: ??
- *   - cloudabi: https://github.com/NuxiNL/cloudlibc/blob/7e5c649/src/include/unistd.h
- *   - wasi: https://github.com/WebAssembly/wasi-libc/blob/575e157/libc-top-half/musl/include/features.h
- *   - emscripten: https://github.com/emscripten-core/emscripten/blob/dee59ba/system/lib/libc/musl/include/features.h
- *   - posix: https://pubs.opengroup.org/onlinepubs/7908799/xsh/compilation.html
- *            https://pubs.opengroup.org/onlinepubs/007904975/functions/xsh_chap02_02.html
+ *   glibc: https://man7.org/linux/man-pages/man7/feature_test_macros.7.html
+ *          https://github.com/bminor/glibc/blob/6c57d32/include/features.h
+ *   bionic: https://github.com/aosp-mirror/platform_bionic/blob/a1112fd/libc/include/sys/cdefs.h#L162
+ *   musl: https://github.com/ifduyue/musl/blob/1febd21/include/features.h
+ *   darwin: https://github.com/apple/darwin-xnu/blob/d4061fb/bsd/sys/cdefs.h#L792
+ *   freebsd: https://github.com/freebsd/freebsd-src/blob/cfad8bd/sys/sys/cdefs.h#L638
+ *   openbsd: https://github.com/openbsd/src/blob/43b1a0f/sys/sys/cdefs.h#L261
+ *   netbsd: https://github.com/NetBSD/src/blob/ee650a6/sys/sys/featuretest.h
+ *   dragonfly: https://github.com/DragonFlyBSD/DragonFlyBSD/blob/c97dc9d/sys/sys/cdefs.h#L597
+ *   sun: https://docs.oracle.com/cd/E19253-01/816-5175/standards-5/index.html
+ *        https://github.com/illumos/illumos-gate/blob/9ecd05b/usr/src/uts/common/sys/feature_tests.h
+ *        https://github.com/illumos/illumos-gate/blob/9ecd05b/usr/src/uts/common/sys/unistd.h
+ *   cygwin: https://github.com/cygwin/cygwin/blob/8050ef2/newlib/libc/include/sys/features.h
+ *   hpux: https://nixdoc.net/man-pages/HP-UX/man5/stdsyms.5.html
+ *   aix: ??
+ *   z/os: https://www.ibm.com/docs/en/zos/2.3.0?topic=files-feature-test-macros
+ *   qnx: http://www.qnx.com/developers/docs/6.5.0_sp1/topic/com.qnx.doc.neutrino_prog/devel.html
+ *        https://github.com/vocho/openqnx/blob/cc95df3/trunk/lib/c/public/sys/platform.h
+ *   haiku: https://github.com/haiku/haiku/blob/144f45a/headers/compatibility/bsd/features.h
+ *          https://github.com/haiku/haiku/blob/144f45a/headers/posix/unistd.h
+ *   minix: https://github.com/Stichting-MINIX-Research-Foundation/minix/blob/4db99f4/sys/sys/featuretest.h
+ *   redox: https://github.com/redox-os/relibc/blob/9790289/include/bits/unistd.h
+ *   vxworks: ??
+ *   cloudabi: https://github.com/NuxiNL/cloudlibc/blob/7e5c649/src/include/unistd.h
+ *   wasi: https://github.com/WebAssembly/wasi-libc/blob/575e157/libc-top-half/musl/include/features.h
+ *   emscripten: https://github.com/emscripten-core/emscripten/blob/dee59ba/system/lib/libc/musl/include/features.h
+ *   posix: https://pubs.opengroup.org/onlinepubs/7908799/xsh/compilation.html
+ *          https://pubs.opengroup.org/onlinepubs/007904975/functions/xsh_chap02_02.html
  */
 
-#if defined(__linux__) || defined(__CYGWIN__)
+#if defined(__linux__)
 #  undef _GNU_SOURCE
 #  define _GNU_SOURCE
-#elif defined(__gnu_hurd__) || (defined(__GNU__) && defined(__MACH__))
+#elif defined(__gnu_hurd__)
 #  undef _GNU_SOURCE
 #  define _GNU_SOURCE
-#elif defined(__FreeBSD_kernel__) && !defined(__FreeBSD__)
+#elif defined(__FreeBSD_kernel__) && defined(__GLIBC__)
 #  undef _GNU_SOURCE
 #  define _GNU_SOURCE
 #elif defined(__sun) && defined(__SVR4)
@@ -65,6 +83,9 @@
 #  define _XOPEN_SOURCE 500
 #  define __EXTENSIONS__
 #  define _REENTRANT
+#elif defined(__CYGWIN__)
+#  undef _GNU_SOURCE
+#  define _GNU_SOURCE
 #elif defined(__hpux)
 #  undef _HPUX_SOURCE
 #  undef _REENTRANT
@@ -91,8 +112,17 @@
 #  define _GNU_SOURCE
 #endif
 
-/*
+/**
  * POSIX Detection
+ *
+ * We can always try to detect POSIX support by testing
+ * `__unix` or `__unix__`. However, a lot of POSIX OSes
+ * are not necessarily unix, and even if they are, a
+ * number of compilers neglect to define the unix macros
+ * anyway.
+ *
+ * Below, we basically keep track of every POSIX OS that
+ * does not have the unix definitions.
  */
 
 #if defined(__unix) || defined(__unix__)
@@ -123,8 +153,8 @@
 /* unix not defined on Clang. */
 #  define TORSION_POSIX
 #elif defined(__EMSCRIPTEN__)
-/* unix not defined on (raw) Clang, but
-   it is defined by emcc since 2016. */
+/* unix not defined on Clang, but
+   is defined by emcc since 2016. */
 #  define TORSION_POSIX
 #endif
 
