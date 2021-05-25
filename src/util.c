123f518125c146b90bb25d378e06398ca0ad2322
Christopher Jeffrey <chjjeffrey@gmail.com>
2021-05-25 14:02:31 -0700

    src/util: style refactor for torsion_memzero.

--- src/util.c
+++ src/util.c
@@ -33,10 +33,16 @@ torsion_memzero(void *ptr, size_t len) {
 #elif defined(TORSION_HAVE_ASM)
   if (len > 0) {
     memset(ptr, 0, len);
-    __asm__ __volatile__ ("" :: "r" (ptr) : "memory");
+
+    __asm__ __volatile__ (
+      ""
+      :: "r" (ptr)
+      : "memory"
+    );
   }
 #else
   static void *(*const volatile memset_ptr)(void *, int, size_t) = memset;
+
   if (len > 0)
     memset_ptr(ptr, 0, len);
 #endif
