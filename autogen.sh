a85a3af795bf62a6f80a0626c222904a7690b487
Christopher Jeffrey <chjjeffrey@gmail.com>
2021-07-24 07:59:42 -0400

    build: add autotools support again.

---
+++ autogen.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# autogen.sh - autotools generation script
+# Copyright (c) 2020, Christopher Jeffrey (MIT License).
+# https://github.com/bcoin-org/libtorsion
+
+set -ex
+
+cd `dirname "$0"`
+
+if type glibtoolize > /dev/null 2>& 1; then
+  glibtoolize --copy --force
+else
+  libtoolize --copy --force
+fi
+
+aclocal --force -I m4
+autoconf --force
+automake --add-missing --copy --force-missing
