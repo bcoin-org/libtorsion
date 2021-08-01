37788d8620b27085be7b93d3dfce636a177d4337
Christopher Jeffrey <chjjeffrey@gmail.com>
2021-08-01 18:48:41 -0400

    build: add -Wcast-align=strict.

--- cmake/AppendCCompilerFlag.cmake
+++ cmake/AppendCCompilerFlag.cmake
@@ -9,11 +9,18 @@ endif()
 include(CheckCCompilerFlag)
 
 function(append_c_compiler_flag list)
+  check_c_compiler_flag(-Werror=unknown-warning-option
+                        CMAKE_HAVE_UNKNOWN_WARNING_OPTION)
+
   foreach(flag ${ARGN})
     string(TOUPPER "CMAKE_HAVE_C_FLAG${flag}" name)
     string(REGEX REPLACE "[^A-Z0-9]" "_" name "${name}")
 
-    check_c_compiler_flag(${flag} ${name})
+    if(CMAKE_HAVE_UNKNOWN_WARNING_OPTION)
+      check_c_compiler_flag("-Werror=unknown-warning-option ${flag}" ${name})
+    else()
+      check_c_compiler_flag(${flag} ${name})
+    endif()
 
     if(${name})
       list(APPEND ${list} ${flag})
